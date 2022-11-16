6fdd340df62f6bc312de158e534b216a7270c317
QuoiQueTuSois <117736359+QuoiQueTuSois@users.noreply.github.com>
2022-11-16 20:51:32 +0900

    update zoom in and out image

--- src/App.ts
+++ src/App.ts
@@ -1,21 +1,69 @@
 export default function App() {
 
-    const img = new Image();
-    img.src = '/src/cavil.png'
-    const canvas: HTMLCanvasElement = document.createElement("canvas")
-    canvas.width = 500;
-    canvas.height = 500;
-    // 그리기
-    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!
-    ctx.beginPath();
-    ctx.moveTo(30, 96);
-    ctx.lineTo(70, 66);
-    ctx.lineTo(103, 76);
-    ctx.lineTo(170, 15);
-    ctx.stroke();
-    ctx.drawImage(img,100,100)
-    ctx.stroke()
-   
+    const zoomIntensity = 0.2;
 
-    return canvas
+
+   
+    const canvas  = <HTMLCanvasElement>document.getElementById('canvas');
+    let context = canvas.getContext("2d");
+    
+    
+    const width = 600;
+    const height = 200;
+    
+    let scale = 1;
+    let originx = 0;
+    let originy = 0;
+    let visibleWidth = width;
+    let visibleHeight = height;
+    
+    
+    function draw(){
+        // Clear screen to white.
+        context.fillStyle = "white";
+        context.fillRect(originx, originy, width/scale, height/scale);
+        // Draw the black square.
+        context.fillStyle = "black";
+        context.fillRect(50, 50, 100, 100);
+    
+        // Schedule the redraw for the next display refresh.
+        window.requestAnimationFrame(draw);
+    }
+    // Begin the animation loop.
+    draw();
+    
+    canvas.onwheel = function (event){
+        event.preventDefault();
+        // Get mouse offset.
+        const mousex = event.clientX - canvas.offsetLeft;
+        const mousey = event.clientY - canvas.offsetTop;
+        // Normalize mouse wheel movement to +1 or -1 to avoid unusual jumps.
+        const wheel = event.deltaY < 0 ? 1 : -1;
+    
+        // Compute zoom factor.
+        const zoom = Math.exp(wheel * zoomIntensity);
+        
+        // Translate so the visible origin is at the context's origin.
+        context.translate(originx, originy);
+      
+        // Compute the new visible origin. Originally the mouse is at a
+        // distance mouse/scale from the corner, we want the point under
+        // the mouse to remain in the same place after the zoom, but this
+        // is at mouse/new_scale away from the corner. Therefore we need to
+        // shift the origin (coordinates of the corner) to account for this.
+        originx -= mousex/(scale*zoom) - mousex/scale;
+        originy -= mousey/(scale*zoom) - mousey/scale;
+        
+        // Scale it (centered around the origin due to the trasnslate above).
+        context.scale(zoom, zoom);
+        // Offset the visible origin to it's proper position.
+        context.translate(-originx, -originy);
+    
+        // Update scale and others.
+        scale *= zoom;
+        visibleWidth = width / scale;
+        visibleHeight = height / scale;
+    }
+    
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,72 @@
 
 import App from "./App"
-document.getElementById("app")!.appendChild(App())
-
-const button = document.getElementById("button");
 
 
-button.addEventListener("click", () => {
-  alert("clicked!");
-});
+//강도설정
+const zoomIntensity = 0.2;
+   
+    const canvas  = <HTMLCanvasElement>document.getElementById('canvas');
+    //캔버스 초기설정
+    canvas.width= 500;
+    canvas.height= 500;    
+    let ctx = canvas.getContext("2d");
 
+    const width = 500;
+    const height = 500;
+    let visibleWidth = width;
+    let visibleHeight = height;
 
+    
+    //줌 스케일
+    let zoomScale = 1;
+    //이동전 좌표
+    let orgX = 0;
+    let orgY = 0;
+
+    
+    //그리는 함수
+    const draw=() =>
+    {
+      //이미지 소스 불러오기
+      const img = new Image();
+      img.width=100;
+      img.height=100;
+      img.src= "https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp"
+       
+      
+      // 캔버스 줌 인아웃시 잔상없애기 
+        // ctx.fillStyle = "white";
+        // ctx.fillRect(orgX, orgY, width/zoomScale, height/zoomScale);
+        ctx.clearRect(orgX, orgY, width/zoomScale, height/zoomScale)
+        // 가져온 이미지 그려넣기
+        ctx.drawImage(img, 0,0, 500, 500);   
+       //새로고침
+        window.requestAnimationFrame(draw);
+    }
+    // 그리기 호출
+    draw();
+    
+
+    //휠 이벤트 설정
+    canvas.onwheel = function (event){
+        event.preventDefault();
+        // 오프셋 설정
+        const mousex = event.clientX - canvas.offsetLeft;
+        const mousey = event.clientY - canvas.offsetTop;
+        // 마우스 튀는현상 방지용
+        const mouseWheel = event.deltaY < 0 ? 1 : -1;
+        // 줌비율계산
+        const zoom = Math.exp(mouseWheel * zoomIntensity);
+        ctx.translate(orgX, orgY);
+        //마우스 올린곳 기준으로 계속 확대되게
+        orgX -= mousex/(zoomScale*zoom) - mousex/zoomScale;
+        orgY -= mousey/(zoomScale*zoom) - mousey/zoomScale;
+        ctx.scale(zoom, zoom);
+        ctx.translate(-orgX, -orgY);
+    
+        // 상태갱신용
+        zoomScale *= zoom;
+        visibleWidth = width / zoomScale;
+        visibleHeight = height / zoomScale;
+    }
+    
